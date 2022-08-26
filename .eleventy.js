1b9a1b86b4fa540cd0e0cef69c14ba36a871b4c3
Stiffpixels <saifimuzammil2@gmail.com>
2022-08-26 06:35:57 +0530

    Adding final pices of code for cms

--- .eleventy.js
+++ .eleventy.js
@@ -1,7 +1,14 @@
+const {DateTime} = require("luxon");
+
 module.exports = function(eleventyConfig){
+  eleventyConfig.addFilter("postDate", (dateobj) => {
+    return DateTime.fromJSDate(dateobj).toLocaleString(DateTime.Date_Med);
+  });
+  
   eleventyConfig.addPassthroughCopy('./src/style.css');
   eleventyConfig.addPassthroughCopy('./src/assets');
-  eleventyConfig.addPassthroughCopy('./src/index.js')
+  eleventyConfig.addPassthroughCopy('./src/index.js');
+  eleventyConfig.addPassthroughCopy('./src/admin');
   return{
     dir:{
       input: "src",
